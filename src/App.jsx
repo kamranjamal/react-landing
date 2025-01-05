87280ae3ee18efa1cc02ccaf9717a08557a57dcf
kamran_jamal <kamrankhanwxr@gmail.com>
2025-01-05 12:44:15 +0500

    hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+import HeroSetion from './compoenents/HeroSetion'
 import Navbar from './compoenents/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <main>
+    <main className=' bg-gray-800 text-gray-100'>
       <Navbar/>
+      <HeroSetion/>
     
     </main>
   )
---
+++ src/compoenents/HeroSetion.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function HeroSetion() {
+  return (
+    <div className="w-full  p-10 flex items-center gap-3 mt-16 flex-col">
+      <h2 className="text-7xl font-bold">Wellcome to Simple Landing Page</h2>
+      <p className="text-center text-gray-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Nostrum excepturi tempora,
+        quam nemo tempore similique laudantium? Molestias accusamus odio
+        reiciendis illo amet fuga quisquam autem nulla magnam. Minus pariatur
+        reprehenderit alias nostrum quibusdam nulla voluptatibus corrupti,
+        officiis, ipsam, ducimus quod eum .
+      </p>
+      <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md px-4 py-2">Get Started</button>
+    </div>
+  );
+}
+
+export default HeroSetion;
